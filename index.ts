d82a94eb49fb919a75a428fa0562380fa7d0ab56
Jaslyn S <7008316+aelesia@users.noreply.github.com>
2020-07-21 02:26:35 +0800

    Export errors

--- index.ts
+++ index.ts
@@ -1,1 +1,2 @@
 export { default } from './src/RedditAPI'
+export { RedditAPIErr } from './src/RedditAPIErr'
--- src/RedditAPIErr.ts
+++ src/RedditAPIErr.ts
@@ -1,49 +1,56 @@
-export namespace RedditAPIErr {
-  export class General extends Error {
-    constructor(msg: string) {
-      super(msg)
-      this.name = 'RedditAPIGeneralError'
-      Object.setPrototypeOf(this, General.prototype)
-    }
+class General extends Error {
+  constructor(msg: string) {
+    super(msg)
+    this.name = 'RedditAPIGeneralError'
+    Object.setPrototypeOf(this, General.prototype)
   }
+}
 
-  export class PostLimit extends General {
-    constructor(msg: string) {
-      super(msg)
-      this.name = 'RedditAPIPostLimitError'
-      Object.setPrototypeOf(this, PostLimit.prototype)
-    }
+class PostLimit extends General {
+  constructor(msg: string) {
+    super(msg)
+    this.name = 'RedditAPIPostLimitError'
+    Object.setPrototypeOf(this, PostLimit.prototype)
   }
+}
 
-  export class ServerBusy extends General {
-    constructor(msg: string) {
-      super(msg)
-      this.name = 'RedditAPIServerBusyError'
-      Object.setPrototypeOf(this, ServerBusy.prototype)
-    }
+class ServerBusy extends General {
+  constructor(msg: string) {
+    super(msg)
+    this.name = 'RedditAPIServerBusyError'
+    Object.setPrototypeOf(this, ServerBusy.prototype)
   }
+}
 
-  export class Failed extends General {
-    constructor(msg: string) {
-      super(msg)
-      this.name = 'RedditAPIFailedError'
-      Object.setPrototypeOf(this, Failed.prototype)
-    }
+class Failed extends General {
+  constructor(msg: string) {
+    super(msg)
+    this.name = 'RedditAPIFailedError'
+    Object.setPrototypeOf(this, Failed.prototype)
   }
+}
 
-  export class Null extends General {
-    constructor(msg: string) {
-      super(msg)
-      this.name = 'RedditAPINullError'
-      Object.setPrototypeOf(this, Null.prototype)
-    }
+class Null extends General {
+  constructor(msg: string) {
+    super(msg)
+    this.name = 'RedditAPINullError'
+    Object.setPrototypeOf(this, Null.prototype)
   }
+}
 
-  export class Unauthorized extends General {
-    constructor(msg: string) {
-      super(msg)
-      this.name = 'RedditAPIUnauthorizedError'
-      Object.setPrototypeOf(this, Unauthorized.prototype)
-    }
+class Unauthorized extends General {
+  constructor(msg: string) {
+    super(msg)
+    this.name = 'RedditAPIUnauthorizedError'
+    Object.setPrototypeOf(this, Unauthorized.prototype)
   }
+}
+
+export const RedditAPIErr = {
+  General,
+  PostLimit,
+  ServerBusy,
+  Failed,
+  Null,
+  Unauthorized
 }
